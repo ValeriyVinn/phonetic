fb080ac1d6b06154065dbaf34ae6587069f19ddf
ValeriyVinn <valeriy.all@gmail.com>
2025-02-28 22:05:50 +0200

    Fixed json data

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // server: {
+  //   open: "/phonetic/", // This will automatically open http://localhost:5173/phonetic/
+  // },
   plugins: [react()],
   base: "/phonetic/",
 });
